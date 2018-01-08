a2a228b2a9020c391344830188be9bb6c0e156f0
Saeid Mohadjer <saeid@fastmail.fm>
2018-01-08 12:03:25 +0100

    refactor and optimisations

--- app/playlist.js
+++ app/playlist.js
@@ -1,49 +1,88 @@
-var player;
-var firstVideoId = '';
+var youtubePlaylist = {
+	playlist: document.querySelector('.youtube-playlist'),
+	getPlaylistId: function() {
+		return this.playlist.getAttribute('data-playlist-id');
+	},
+	getApiKey: function() {
+		return this.playlist.getAttribute('data-api-key');
+	},
+	updateVideo: function(e) {
+		const img = e.target;
+		const elements = document.querySelectorAll('.youtube-playlist-list li img');
+
+		Array.prototype.forEach.call(elements, function(el, i){
+			el.classList.remove('selected');
+		});
+
+		img.classList.add('selected');
+
+		const videoId = img.getAttribute('data-id');
+		youtubePlaylist.player.loadVideoById(videoId);
+	},
+	addItem: function(snippet) {
+		const list = document.getElementsByClassName('youtube-playlist-list')[0];
+		const thumbnail = snippet.thumbnails.default.url;
+		const videoId = snippet.resourceId.videoId;
+		const img = `<img data-id="${videoId}" src="${thumbnail}" />`;
+		const li = document.createElement('li');
+
+		li.innerHTML = img;
+		li.addEventListener('click', youtubePlaylist.updateVideo);
+		list.appendChild(li);
+	},
+	loadAPI: function() {
+		 var tag = document.createElement('script');
+		 tag.src = "https://www.youtube.com/iframe_api";
+		 var firstScriptTag = document.getElementsByTagName('script')[0];
+		 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+	}
+};
 
 function onYouTubeIframeAPIReady() {
-	console.log('api ready');
-	player = new YT.Player('player', {
-		videoId: firstVideoId,
+	const list = youtubePlaylist.playlist.querySelector('.youtube-playlist-list');
+	const img = list.querySelector('li img');
+	const videoId = img.getAttribute('data-id');
+
+	img.classList.add('selected');
+
+	youtubePlaylist.player = new YT.Player(document.querySelector('.youtube-playlist-player'), {
+		videoId: videoId,
+		playerVars: {
+			'autoplay': 0,
+			'rel': 0,
+			'showinfo': 0
+		},
 		events: {
-			//'onReady': onPlayerReady,
-		//	'onStateChange': onPlayerStateChange
+			'onReady': onPlayerReady,
+			'onStateChange': onPlayerStateChange
 		}
 	});
 }
 
-function loadYoutubeAPI() {
-		// 2. This code loads the IFrame Player API code asynchronously.
-	 var tag = document.createElement('script');
+function onPlayerReady(e) {
+	//console.log(e);
+}
 
-	 tag.src = "https://www.youtube.com/iframe_api";
-	 var firstScriptTag = document.getElementsByTagName('script')[0];
-	 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+function onPlayerStateChange(e) {
+	//console.log(e);
 }
 
 function onGoogleLoad() {
-	gapi.client.setApiKey('AIzaSyDqnBq55I_HGGdNdyVL94VqrFwLSqy7OqI');
+	gapi.client.setApiKey(youtubePlaylist.getApiKey());
 	gapi.client.load('youtube', 'v3', function() {
-
-		var request = gapi.client.youtube.playlistItems.list({
+		const request = gapi.client.youtube.playlistItems.list({
 			part: 'snippet',
-			playlistId: 'FL5lXztBly8UeivS8M3uQbxA',
+			playlistId: youtubePlaylist.getPlaylistId(),
 			maxResults: 50
 		});
 
 		request.execute(function(response) {
 			for (var i = 0; i < response.items.length; i++) {
 				var snippet = response.items[i].snippet;
-				console.log(snippet.resourceId.videoId);
-
-				if (i === 0) {
-					firstVideoId = snippet.resourceId.videoId;
-					console.log(firstVideoId);
-					//player.playVideo(firstVideoId);
-				}
+				youtubePlaylist.addItem(snippet);
 			}
 
-			loadYoutubeAPI();
+			youtubePlaylist.loadAPI();
 		});
 	});
 }
