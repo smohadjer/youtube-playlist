2c65375c3d36c0bbfab840d3b429bdb68699b11c
Saeid Mohadjer <saeid@fastmail.fm>
2018-01-08 13:45:33 +0100

    refactor

--- app/playlist.js
+++ app/playlist.js
@@ -1,26 +1,57 @@
 var youtubePlaylist = {
-	playlist: document.querySelector('.youtube-playlist'),
-	getPlaylistId: function() {
-		return this.playlist.getAttribute('data-playlist-id');
+	getPlaylistId: function(list) {
+		return list.getAttribute('data-playlist-id');
 	},
-	getApiKey: function() {
-		return this.playlist.getAttribute('data-api-key');
+	getApiKey: function(list) {
+		return list.getAttribute('data-api-key');
+	},
+	init: function() {
+		youtubePlaylist.lists = document.querySelectorAll('.youtube-playlist');
+		let done = false;
+
+		youtubePlaylist.lists.forEach(function(list, index) {
+
+			gapi.client.setApiKey(youtubePlaylist.getApiKey(list));
+			gapi.client.load('youtube', 'v3', function() {
+				const request = gapi.client.youtube.playlistItems.list({
+					part: 'snippet',
+					playlistId: youtubePlaylist.getPlaylistId(list),
+					maxResults: 50
+				});
+
+				request.execute(function(response) {
+					for (let i = 0; i < response.items.length; i++) {
+						var snippet = response.items[i].snippet;
+						youtubePlaylist.addItem(list, snippet);
+					}
+
+					if (!done) {
+						done = true;
+						youtubePlaylist.loadAPI();
+					}
+				});
+			});
+		});
 	},
 	updateVideo: function(e) {
 		const img = e.target;
-		const elements = document.querySelectorAll('.youtube-playlist-list li img');
+		const li = img.parentNode;
+		const ul = li.parentNode;
+		const list = ul.parentNode;
+		const thumbs = ul.querySelectorAll('li img');
 
-		Array.prototype.forEach.call(elements, function(el, i){
+		Array.prototype.forEach.call(thumbs, function(el, i){
 			el.classList.remove('selected');
 		});
 
 		img.classList.add('selected');
 
 		const videoId = img.getAttribute('data-id');
+
 		youtubePlaylist.player.loadVideoById(videoId);
 	},
-	addItem: function(snippet) {
-		const list = document.getElementsByClassName('youtube-playlist-list')[0];
+	addItem: function(list, snippet) {
+		ul = list.querySelector('.youtube-playlist-list');
 		const thumbnail = snippet.thumbnails.default.url;
 		const videoId = snippet.resourceId.videoId;
 		const img = `<img data-id="${videoId}" src="${thumbnail}" />`;
@@ -28,34 +59,38 @@ var youtubePlaylist = {
 
 		li.innerHTML = img;
 		li.addEventListener('click', youtubePlaylist.updateVideo);
-		list.appendChild(li);
+		ul.appendChild(li);
 	},
 	loadAPI: function() {
-		 var tag = document.createElement('script');
-		 tag.src = "https://www.youtube.com/iframe_api";
-		 var firstScriptTag = document.getElementsByTagName('script')[0];
-		 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+		var tag = document.createElement('script');
+		tag.src = "https://www.youtube.com/iframe_api";
+		var firstScriptTag = document.getElementsByTagName('script')[0];
+		firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 	}
 };
 
+gapi.load('client', youtubePlaylist.init);
+
 function onYouTubeIframeAPIReady() {
-	const list = youtubePlaylist.playlist.querySelector('.youtube-playlist-list');
-	const img = list.querySelector('li img');
-	const videoId = img.getAttribute('data-id');
+	youtubePlaylist.lists.forEach(function(list, index) {
+		const ul = list.querySelector('.youtube-playlist-list');
+		const img = ul.querySelector('li img');
+		const videoId = img.getAttribute('data-id');
+		img.classList.add('selected');
 
-	img.classList.add('selected');
+		youtubePlaylist.player = new YT.Player(list.querySelector('.youtube-playlist-player'), {
+			videoId: videoId,
+			playerVars: {
+				'autoplay': 0,
+				'rel': 0,
+				'showinfo': 0
+			},
+			events: {
+				'onReady': onPlayerReady,
+				'onStateChange': onPlayerStateChange
+			}
+		});
 
-	youtubePlaylist.player = new YT.Player(document.querySelector('.youtube-playlist-player'), {
-		videoId: videoId,
-		playerVars: {
-			'autoplay': 0,
-			'rel': 0,
-			'showinfo': 0
-		},
-		events: {
-			'onReady': onPlayerReady,
-			'onStateChange': onPlayerStateChange
-		}
 	});
 }
 
@@ -65,24 +100,4 @@ function onPlayerReady(e) {
 
 function onPlayerStateChange(e) {
 	//console.log(e);
-}
-
-function onGoogleLoad() {
-	gapi.client.setApiKey(youtubePlaylist.getApiKey());
-	gapi.client.load('youtube', 'v3', function() {
-		const request = gapi.client.youtube.playlistItems.list({
-			part: 'snippet',
-			playlistId: youtubePlaylist.getPlaylistId(),
-			maxResults: 50
-		});
-
-		request.execute(function(response) {
-			for (var i = 0; i < response.items.length; i++) {
-				var snippet = response.items[i].snippet;
-				youtubePlaylist.addItem(snippet);
-			}
-
-			youtubePlaylist.loadAPI();
-		});
-	});
 }
